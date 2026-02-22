0a3d9673de5c11714e1001b01fabaed068245ac6
wailadivyansh <76157070+wailadivyansh@users.noreply.github.com>
2026-02-22 17:39:15 +0530

    Add files via upload

---
+++ day22.2.c
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+struct ListNode* reverseList(struct ListNode* head) {
+    struct ListNode *prev = NULL;
+    struct ListNode *curr = head;
+    struct ListNode *nextNode = NULL;
+    
+    while(curr != NULL) {
+        nextNode = curr->next;  
+        curr->next = prev;      
+        prev = curr;            
+        curr = nextNode;       
+    }
+    
+    return prev; 
+}
