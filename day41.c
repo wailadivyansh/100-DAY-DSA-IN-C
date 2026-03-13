376586eb9acd1e82289ca42768a68718842cfd39
wailadivyansh <76157070+wailadivyansh@users.noreply.github.com>
2026-03-13 16:09:23 +0530

    Create day41.c

---
+++ day41.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node *front = NULL, *rear = NULL;
+
+void enqueue(int x) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = x;
+    temp->next = NULL;
+
+    if (rear == NULL) {
+        front = rear = temp;
+        return;
+    }
+
+    rear->next = temp;
+    rear = temp;
+}
+
+void dequeue() {
+    if (front == NULL) {
+        printf("-1\n");
+        return;
+    }
+
+    struct Node* temp = front;
+    printf("%d\n", front->data);
+
+    front = front->next;
+
+    if (front == NULL)
+        rear = NULL;
+
+    free(temp);
+}
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    char op[10];
+    int x;
+
+    for (int i = 0; i < N; i++) {
+        scanf("%s", op);
+
+        if (op[0] == 'e') {   // enqueue
+            scanf("%d", &x);
+            enqueue(x);
+        } else if (op[0] == 'd') {   // dequeue
+            dequeue();
+        }
+    }
+
+    return 0;
+}
